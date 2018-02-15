c3ed0f43ba612df230b3a637a83ccd11a0886b4e
Gaiwer <gaiwer@meta.ua>
2018-02-15 10:36:42 +0200

    It works!

--- src/App.js
+++ src/App.js
@@ -67,23 +67,26 @@ class AuthButton  extends Component {
     constructor(props) {
         super(props);
 
+        // this.state = {
+        //     status:[]
+        // }
+
         this.state = {
-            status:[]
-        }
+            error: null,
+            isLoaded: false,
+            data: []
+        };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.findRepository = this.findRepository.bind(this);
     }
 
-
-
     login = () => {
         fakeAuth.authenticate(() => {
             this.setState({ redirectToReferrer: true });
         });
     }
-
 
     searchHandler = (event) => {
         event.preventDefault();
@@ -130,60 +133,78 @@ class AuthButton  extends Component {
                         // Examine the text in the response
                         response.json().then((data) => {
                             console.log(data);
-
                             this.setState({
+                                isLoaded: true,
                                 status:data
                             });
                         });
                     }
                 )
+
+
                 .catch(function(err) {
                     console.log('Fetch Error :-S', err);
                 });
-            }
+        }
     }
 
     render() {
-        console.log(this.state)
-        // console.log(this.state.status[0].id)
+        console.log(this.state);
+        const { error, isLoaded, data } = this.state;
+        const answer = this.state.status;
+        console.log(answer);
 
-        return(
-            fakeAuth.isAuthenticated ? (
+        if (isLoaded) {
+            return (
                 <div>
-                    Welcome!{" "}
-                    <div className='col-md-12'>Найдите репозиторий по имени владельца:</div>
-                    <form className='col-md-4' onSubmit={this.findRepository}>
-                        <input type='text' placeholder='Enter name of owner' onChange={this.handleChange}/>
-                        <button type='submit'>Enter</button>
-                    </form>
-
-
-                    {/*<ul>*/}
-                        {/*{this.state.status[0].id}*/}
-                    {/*</ul>*/}
-
-
-                    <button onClick={() => {
-                        location.reload();
-                    }}
-                    >
-                        Sign out
-                    </button>
+                    { answer.map( (el, key) => (
+                        <li
+                            key={key}
+                            link={el.url}
+                        >
+                            {el.name}
+                        </li>
+                    ) ) }
                 </div>
-            ) : (
-                <div>
-                    <p>You are not logged in.</p>
-                    <p>You must log in to view the protected page.</p>
-                    <div className='row'>
-                        <div className='col-md-12'>Please enter your login:</div>
-                        <form className='col-md-4' onSubmit={this.handleSubmit}>
-                            <input type='text' placeholder='Enter your login'/>
-                            <button type='submit' onClick={this.login}>Enter</button>
+            );
+        } else {
+            return (
+                fakeAuth.isAuthenticated ? (
+                    <div>
+                        Welcome!{" "}
+                        <div className='col-md-12'>Найдите репозиторий по имени владельца:</div>
+                        <form className='col-md-4' onSubmit={this.findRepository}>
+                            <input type='text' placeholder='Enter name of owner' onChange={this.handleChange}/>
+                            <button type='submit'>Enter</button>
                         </form>
+
+
+                        {/*<ul>*/}
+                        {/*{this.state.status[0].id}*/}
+                        {/*</ul>*/}
+
+                        <button onClick={() => {
+                            location.reload();
+                        }}
+                        >
+                            Sign out
+                        </button>
                     </div>
-                </div>
-            )
-        );
+                ) : (
+                    <div>
+                        <p>You are not logged in.</p>
+                        <p>You must log in to view the protected page.</p>
+                        <div className='row'>
+                            <div className='col-md-12'>Please enter your login:</div>
+                            <form className='col-md-4' onSubmit={this.handleSubmit}>
+                                <input type='text' placeholder='Enter your login'/>
+                                <button type='submit' onClick={this.login}>Enter</button>
+                            </form>
+                        </div>
+                    </div>
+                )
+            );
+        }
     }
 }
 
